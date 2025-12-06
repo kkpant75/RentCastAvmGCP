e99991b88488e050de7b378e54337a87139b0d6f
kkpant75 <76734850+kkpant75@users.noreply.github.com>
2025-12-07 00:26:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from RentCastAVM import *
 def rentcast_avm_processor(request):
     
     # Obtain API key from environment variables for Google Cloud Function
-    RENTCAST_API_KEY = "" #GetRentCastAPIKeyFromSecrets()
+    RENTCAST_API_KEY = os.getenv("RENTCAST_API_KEY") #GetRentCastAPIKeyFromSecrets()
     COMP_COUNT = 5  # Number of comparables (can be parameterized)
 
     # Instantiate the processor with the API key and comp count
@@ -16,5 +16,5 @@ def rentcast_avm_processor(request):
     # Run the address processing logic
     processor.process_all_addresses()
     
-    return "Processing completed."
+    return "Processing completed...." +str(RENTCAST_API_KEY)
  
