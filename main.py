899b6fce548a4b1ccc4c8a47324c6bf901b777f6
kkpant75 <76734850+kkpant75@users.noreply.github.com>
2025-12-06 23:56:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import functions_framework
 @functions_framework.http
 def rentcast_avm_processor(request):
     
-    # # Obtain API key from environment variables for Google Cloud Function
-    # RENTCAST_API_KEY = GetRentCastAPIKeyFromSecrets()
-    # COMP_COUNT = 5  # Number of comparables (can be parameterized)
-
+    # Obtain API key from environment variables for Google Cloud Function
+    RENTCAST_API_KEY = GetRentCastAPIKeyFromSecrets()
+    COMP_COUNT = 5  # Number of comparables (can be parameterized)
+    print(RENTCAST_API_KEY)
     # # Instantiate the processor with the API key and comp count
     # processor = RentCastAVMProcessor(api_key=RENTCAST_API_KEY, comp_count=COMP_COUNT)
     
